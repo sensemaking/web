3d39db1f6e2d4a759ee8fc100f4b01e873458787
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-06-14 13:20:49 +0100

    _

--- js/core/src/http.spec.ts
+++ js/core/src/http.spec.ts
@@ -1,15 +1,15 @@
 import { describe, test, expect, afterAll, afterEach, beforeAll } from '@jest/globals'
 import 'whatwg-fetch'
 import { setupServer } from 'msw/node'
-import { rest } from 'msw'
-import * as http from './http'
+import { rest as mock } from 'msw'
+import { get, put, post, del } from './http'
 
-const server  = setupServer(
-    rest.get(`*`, async (req, res, ctx) => { 
-        console.log(`No mocked handler setup for ${req.url}`) 
-        return res(ctx.status(404))
-    })
-)
+const noHandler = async (req : any, res : any, ctx : any) => { 
+    console.log(`No mocked handler setup for ${req.method} at ${req.url}`) 
+    return res(ctx.status(404))
+}
+
+const server  = setupServer(mock.get(`*`, noHandler), mock.put(`*`, noHandler), mock.post(`*`, noHandler), mock.delete(`*`, noHandler))
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
@@ -18,48 +18,50 @@ afterAll(() => server.close())
 const url = `http://myapi.com/`
 const jsonMediaType = `application/json`;
 
-describe(`All http requests`, () => {
-    test(`Add an accept header for application/json`, async () => {        
-        server.use(
-            rest.get(`${url}`, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            rest.put(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            rest.post(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            rest.delete(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500)))
-        )
-
-        await http.get(url)
-        await http.put(url, {})
-        await http.post(url, {})
-        await http.del(url)
-    })
-})
-
-describe(`Put & Post`, () => {
-    test(`Add a content-type header for application/json`, async () => { 
-        server.use(
-            rest.put(url, async (req, res, ctx) => res(req.headers.get(`Content-Type`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            rest.post(url, async (req, res, ctx) => res(req.headers.get(`Content-Type`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-        )
-
-        await http.put(url, {})
-        await http.post(url, {})
-    })
-})
-
-describe(`Get`, () => {
+describe(`Metohds`, () => {
     test(`GETs from a url`, async () => {        
         const result = { wibble: `wobble` }
         server.use(
-            rest.get(`${url}`, async (req, res, ctx) => res(ctx.status(200), ctx.json(result))))
+            mock.get(url, async (req, res, ctx) => res(ctx.status(200), ctx.json(result))))
 
-        return expect(await http.get(url)).toEqual(result)
+        return expect(await get(url)).toEqual(result)
+    })
+
+    test(`PUTs a body to a url`, async () => { })
+    test(`POSTs a body to a url`, async () => { })
+
+    test(`DELETEs from a url`, async () => { 
+        server.use(
+            mock.delete(url, async (req, res, ctx) => res(ctx.status(200))))
+
+        await del(url)
     })
 })
 
-describe(`Put`, () => {
-    test(`PUTs a body at a url`, async () => { })
+describe(`Headers`, () => {
+    test(`All http requests add accept header for application/json`, async () => {        
+        server.use(
+            mock.get(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
+            mock.put(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
+            mock.post(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
+            mock.delete(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500)))
+        )
+
+        await get(url)
+        await put(url, {})
+        await post(url, {})
+        await del(url)
+    })
+
+    test(`PUT & POST add content-type header for application/json`, async () => { 
+        server.use(
+            mock.put(url, async (req, res, ctx) => res(req.headers.get(`Content-Type`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
+            mock.post(url, async (req, res, ctx) => res(req.headers.get(`Content-Type`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
+        )
+
+        await put(url, {})
+        await post(url, {})
+    })
 })
 
-describe(`Post`, () => {})
-describe(`Del`, () => {})
 describe(`Error Handling`, () => {})
