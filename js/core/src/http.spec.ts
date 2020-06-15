92c5068efea981b7c6c34108a530bd37d67a724e
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-06-15 11:46:43 +0100

    _

--- js/core/src/http.spec.ts
+++ js/core/src/http.spec.ts
@@ -16,36 +16,40 @@ afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 const url = `http://myapi.com/`
-const jsonMediaType = `application/json`;
 
-describe(`Metohds`, () => {
+describe(`Methods`, () => {
     test(`GETs from a url`, async () => {        
         const result = { wibble: `wobble` }
-        server.use(
-            mock.get(url, async (req, res, ctx) => res(ctx.status(200), ctx.json(result))))
+        server.use(mock.get(url, async (req, res, ctx) => res(ctx.status(200), ctx.json(result))))
 
         return expect(await get(url)).toEqual(result)
     })
 
-    test(`PUTs a body to a url`, async () => { })
-    test(`POSTs a body to a url`, async () => { })
+    test(`PUTs a body to a url`, async () => { 
+        const body = { wibble: `wobble` }
+        server.use(mock.put(url, async (req, res, ctx) => { return res((req.body as any).wibble === body.wibble ? ctx.status(200) : ctx.status(500))}))
+
+        await put(url, body)
+    })
+
+    test(`POSTs a body to a url`, async () => { 
+        const body = { wibble: `wobble` }
+        server.use(mock.post(url, async (req, res, ctx) => { return res((req.body as any).wibble === body.wibble ? ctx.status(200) : ctx.status(500))}))
+
+        await post(url, body)
+    })
 
     test(`DELETEs from a url`, async () => { 
-        server.use(
-            mock.delete(url, async (req, res, ctx) => res(ctx.status(200))))
-
+        server.use(mock.delete(url, async (req, res, ctx) => res(ctx.status(200))))
         await del(url)
     })
 })
 
 describe(`Headers`, () => {
+    const checkForJson = (header : string) => (req : any, res : any, ctx : any) => res(req.headers.get(header) === `application/json` ? ctx.status(200) : ctx.status(500))
+
     test(`All http requests add accept header for application/json`, async () => {        
-        server.use(
-            mock.get(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            mock.put(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            mock.post(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            mock.delete(url, async (req, res, ctx) => res(req.headers.get(`Accept`) === jsonMediaType ? ctx.status(200) : ctx.status(500)))
-        )
+        server.use(mock.get(url, checkForJson(`Accept`)), mock.put(url, checkForJson(`Accept`)), mock.post(url, checkForJson(`Accept`)), mock.delete(url, checkForJson(`Accept`)))
 
         await get(url)
         await put(url, {})
@@ -54,10 +58,7 @@ describe(`Headers`, () => {
     })
 
     test(`PUT & POST add content-type header for application/json`, async () => { 
-        server.use(
-            mock.put(url, async (req, res, ctx) => res(req.headers.get(`Content-Type`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-            mock.post(url, async (req, res, ctx) => res(req.headers.get(`Content-Type`) === jsonMediaType ? ctx.status(200) : ctx.status(500))),
-        )
+        server.use(mock.put(url, checkForJson(`Content-Type`)), mock.post(url, checkForJson(`Content-Type`)))
 
         await put(url, {})
         await post(url, {})
