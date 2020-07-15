843e2f3f88837a181c7ec4612c8df909c26daa87
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-07-15 16:55:21 +0100

    _

---
+++ .net/Host/Middleware/ServiceNotification.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using NodaTime;
+using Sensemaking.Host.Monitoring;
+
+namespace Sensemaking.Host.Web
+{
+    internal static class ServiceNotification
+    {
+        internal static IApplicationBuilder UseStatusNotification(this IApplicationBuilder app, IMonitorServices monitor)
+        {
+            Notifier = new ServiceStatusNotifier(monitor);
+            return app;
+        }
+
+        internal static ServiceStatusNotifier? Notifier { get; set; }
+    } 
+}
